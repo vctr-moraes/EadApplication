04e78cb4b308a3da58be56dad49dff02c51c22d8
Victor Moraes <victor.moraes@outlook.com.br>
2026-06-22 12:51:57 -0300

    [course] Implement delete functionality in CourseServiceImpl and ModuleServiceImpl, enhancing cascading delete behavior for modules and lessons. Add new query in LessonRepository to retrieve lessons by module.

--- course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,15 +1,35 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ModuleServiceImpl implements ModuleService {
 
     final ModuleRepository moduleRepository;
+    final LessonRepository lessonRepository;
 
-    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+    public ModuleServiceImpl(ModuleRepository moduleRepository, LessonRepository lessonRepository) {
         this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+
+        if (!lessonModelList.isEmpty()) {
+            lessonRepository.deleteAll(lessonModelList);
+        }
+
+        moduleRepository.delete(moduleModel);
     }
 }
