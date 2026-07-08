f1056317ee1588298ebd3eb0f01ae6e832d6abb1
Victor Moraes <victor.moraes@outlook.com.br>
2026-07-08 15:33:39 -0300

    [course] Implement handling for HttpMessageNotReadableException in GlobalExceptionHandler to provide detailed error responses for invalid enum values.

--- course/src/main/java/com/ead/course/exceptions/GlobalExceptionHandler.java
+++ course/src/main/java/com/ead/course/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.ead.course.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -30,6 +32,24 @@ public class GlobalExceptionHandler {
         });
 
         var errorResponse = new ErrorRecordResponse(HttpStatus.BAD_REQUEST.value(), "Error: Validation failed", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorRecordResponse> handleInvalidFormatException(HttpMessageNotReadableException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        if (ex.getCause() instanceof InvalidFormatException) {
+            InvalidFormatException ifx = (InvalidFormatException) ex.getCause();
+
+            if (ifx.getTargetType() != null && ifx.getTargetType().isEnum()) {
+                String fieldName = ifx.getPath().get(ifx.getPath().size()-1).getFieldName();
+                String errorMessage = ex.getMessage();
+                errors.put(fieldName, errorMessage);
+            }
+        }
+
+        var errorResponse = new ErrorRecordResponse(HttpStatus.BAD_REQUEST.value(), "Error: Invalid enum value", errors);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
