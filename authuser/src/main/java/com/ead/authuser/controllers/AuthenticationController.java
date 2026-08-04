19675f763f204112a0d3f4067e2e6abd0f848181
Victor Moraes <victor.moraes@outlook.com.br>
2026-08-03 22:29:06 -0300

    [authuser] Refactor AuthenticationController imports for better organization; add logging configuration to application.yaml for enhanced debugging.

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,8 +1,7 @@
 package com.ead.authuser.controllers;
 
-import com.ead.authuser.dtos.UserRecordDto;
-import com.ead.authuser.services.UserService;
-import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.authuser.dtos.UserRecordDto;
+import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     final UserService userService;
 
